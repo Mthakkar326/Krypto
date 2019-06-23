a3ca56bb7f0bcf799359d15e4990d2c4ae1f8608
Milan <43902071+Mthakkar326@users.noreply.github.com>
2019-06-23 08:03:36 -0400

    Update Krypto_Game.py

--- Krypto_Game.py
+++ Krypto_Game.py
@@ -9,7 +9,7 @@ def deck_construction():
     # BUILD & SHUFFLE THE DECK
     krypto_deck = (list(range(1,7))*3) + (list(range(7,11))*4) + (list(range(11,18))*2) + list(range(18,26))
     random.shuffle(krypto_deck)
-    return krypto_deck
+    return krypto_deck# KRYPTO GAME
 
 def print_format(print_input, print_output):
     # PRINT FORMAT AS CARDS
@@ -17,24 +17,12 @@ def print_format(print_input, print_output):
     card_top2 = "|    |"
     card_bottom = "|____|"
     spacing = "\t    "
-    print_hand = (5 * card_top + "\n" + 5 * card_top2 + 
+    print_hand = (5 * card_top + "\n" + 5 * card_top2 +
           f"\n|{print_input[0]:^4}||{print_input[1]:^4}||{print_input[2]:^4}||{print_input[3]:^4}||{print_input[4]:^4}|\n"
-          + 5 * card_bottom + "\n" + spacing + card_top + "\n" + spacing + card_top2 + "\n" + spacing + 
+          + 5 * card_bottom + "\n" + spacing + card_top + "\n" + spacing + card_top2 + "\n" + spacing +
           f"|{print_output:^4}|" + "\n" + spacing + card_bottom + "\n")
     return print_hand
 
-def game_instructions():
-    # PRINT GAME INSTRUCTIONS
-    print("Krypto is a mathematical card game with very simple rules. "\
-          "The Krypto deck contains 56 cards labelled 1 through 25. "\
-          "Five cards are randomly dealt in a straight line, known as the input cards. "\
-          "An example is included below:\n[6] [10] [7] [2] [11]\n\n"\
-          "A sixth card is then randomly dealt, known as the target card:\n[6] [10] [7] [2] [11]\n[4]\n\n"\
-          "The objective of the game is to use all 5 input cards exactly once to achieve the target card, using binary operations "\
-          "(addition, subtraction, multiplication and division):\n(6 - 10 / 2) * (11 - 7) = 4")
-    print(krypto_hand)
-    user_solution(krypto_input, krypto_target)
-       
 def check_solution(user_response, krypto_start, krypto_end):
     # CHECK IF RESPONSE CAN BE EVALUATED
     try:
@@ -43,7 +31,7 @@ def check_solution(user_response, krypto_start, krypto_end):
         print("\nYou have entered an invalid equation.")
         user_solution(krypto_start, krypto_end)
     else:
-   
+
         # CHECK IF RESPONSE USES ONLY BINARY OPERATORS
         user_input_sym = user_response
         user_input_sym = list(map(str,re.split("[1234567890]", user_input_sym)))     # splits into list of operators
@@ -52,9 +40,9 @@ def check_solution(user_response, krypto_start, krypto_end):
             print("\nYou can only use binary operations.")
             user_solution(krypto_start, krypto_end)
         else:
-            
-            # CHECK IF RESPONSE USES CORRECT NUMBERS               
-            user_input_num = user_response       
+
+            # CHECK IF RESPONSE USES CORRECT NUMBERS
+            user_input_num = user_response
             user_input_num = list(map(str,re.split("[()+-/*]", user_input_num)))     # splits into list of numbers
             user_input_num = list(filter(None, user_input_num))     # removes blanks
             user_input_num = list(map(int, user_input_num))     # converts list elements to integers
@@ -64,8 +52,8 @@ def check_solution(user_response, krypto_start, krypto_end):
                 print("\nYou did not use the right input numbers.")
                 user_solution(krypto_start, krypto_end)
             else:
-                
-                # CHECK IF RESPONSE EQUALS TARGET               
+
+                # CHECK IF RESPONSE EQUALS TARGET
                 if eval(user_response) == krypto_end:
                     print("\nYou are correct!")
                     play_again()
@@ -76,9 +64,9 @@ def check_solution(user_response, krypto_start, krypto_end):
 def krypto_solver(solve_input, solve_output):
     # BRUTE FORCE SOLVER
     kp = list(permutations(solve_input))
-    op = list(set(list(permutations(['+','+','+','+','-','-','-','-','/','/','/','/','*','*','*','*'],4)))) 
+    op = list(set(list(permutations(['+','+','+','+','-','-','-','-','/','/','/','/','*','*','*','*'],4))))
     solved = False
-    
+
     # GENERATING ALL PERMUTATIONS OF INPUTS, BINARY OPERATORS, PARENTHESES
     for n in range(len(kp)):
         for p in range(len(op)):
@@ -97,8 +85,8 @@ def krypto_solver(solve_input, solve_output):
             potential_solution12 = ''.join(['((',str(kp[n][0]),op[p][0],str(kp[n][1]),')',op[p][1],'(',str(kp[n][2]),op[p][2],str(kp[n][3]),'))',op[p][3],str(kp[n][4])])
             potential_solution13 = ''.join(['((',str(kp[n][0]),op[p][0],'(',str(kp[n][1]),op[p][1],str(kp[n][2]),'))',op[p][2],str(kp[n][3]),')',op[p][3],str(kp[n][4])])
             potential_solution14 = ''.join(['(((',str(kp[n][0]),op[p][0],str(kp[n][1]),')',op[p][1],str(kp[n][2]),')',op[p][2],str(kp[n][3]),')',op[p][3],str(kp[n][4])])
-            
-            # CHECKING IF ANY SOLUTIONS EQUAL THE TARGET. THE TRY SKIPS DIVIDE BY ZERO ERRORS            
+
+            # CHECKING IF ANY SOLUTIONS EQUAL THE TARGET. THE TRY SKIPS DIVIDE BY ZERO ERRORS
             try:
                 if eval(potential_solution) == solve_output:
                     print(potential_solution, " = ", solve_output)
@@ -196,6 +184,17 @@ def krypto_solver(solve_input, solve_output):
         print("No solution exists.")
         play_again()
 
+def game_instructions():
+    # PRINT GAME INSTRUCTIONS
+    print("\nGAME INSTRUCTIONS\n\n"\
+          "Krypto is a mathematical card game with very simple rules. "\
+          "The Krypto deck contains 56 cards labelled 1 through 25. "\
+          "Five cards are randomly dealt in a straight line, known as the input cards. "\
+          "An example is included below:\n[6] [10] [7] [2] [11]\n\n"\
+          "A sixth card is then randomly dealt, known as the target card:\n[6] [10] [7] [2] [11]\n[4]\n\n"\
+          "The objective of the game is to use all 5 input cards exactly once to achieve the target card, using binary operations "\
+          "(addition, subtraction, multiplication and division):\n(6 - 10 / 2) * (11 - 7) = 4\n\n---\n")
+
 def krypto_game():
     # PRINT KRYPTO HAND
     global krypto_input
@@ -213,17 +212,19 @@ def user_solution():
     user_input = input("Enter your response here: ")
     user_input = ''.join(user_input.split())
     user_input = user_input.lower()
-    
-    # EVALUATE USER INPUT    
+
+    # EVALUATE USER INPUT
     if user_input == 'help':
         game_instructions()
+        print(krypto_hand)
+        user_solution()
     elif user_input == 'reset':
         krypto_game()
     elif user_input == 'solve':
         krypto_solver(krypto_input, krypto_target)
     elif user_input == 'quit':
         sys.exit()
-    else:     
+    else:
         check_solution(user_input, krypto_input, krypto_target)
 
 def play_again():
